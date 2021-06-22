51d47fcc1927b1721992c9d5f5d2ba8048bda0c5
Jannik-HHN <maplejannik3@web.de>
2021-06-22 15:09:37 +0200

    (WIP) First carousel prototype
    Just a prototype!!

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function Movie(director, year, actors, description, url) {
 
 function MovieList(name, movies){
     this.name = name;
-    this. movies = movies;
+    this.movies = ko.observableArray(movies);
 }
 
 function Page(name, lists){
@@ -18,21 +18,23 @@ function Page(name, lists){
 
 
 function NetflixViewModel() {
-    this.newMovies = ko.observableArray([
+    this.newMovies = new MovieList("Neue Filme", [
         new Movie("John", 2003, ["Luke", "Sam", "Sarah"], "Luke, Sam und Sarah gehen auf die gleiche Schule. Nach 20 Jahren treffen sie sich wieder bei einem Klassentreffen.", "img/placeholder.png"),
         new Movie("Tom", 2012, ["Tim", "Jim", "Guy"], "Test text, um die Website zu testen. Diese Informationen haben nichts mit dem Film selbst zu tun, es handelt sich lediglich um einen Platzhalter.", "img/placeholder.png"),
         new Movie("Tom", 2012, ["Tim", "Jim", "Guy"], "Test text, um die Website zu testen. Diese Informationen haben nichts mit dem Film selbst zu tun, es handelt sich lediglich um einen Platzhalter.", "img/placeholder.png")
     ]);
-    this.popularMovies = ko.observableArray([
+    this.popularMovies = new MovieList("Derzeit beliebt", [
         new Movie("Tom", 2012, ["Tim", "Jim", "Guy"], "Test text, um die Website zu testen. Diese Informationen haben nichts mit dem Film selbst zu tun, es handelt sich lediglich um einen Platzhalter.", "img/placeholder.png"),
         new Movie("Tom", 2012, ["Tim", "Jim", "Guy"], "Test text, um die Website zu testen. Diese Informationen haben nichts mit dem Film selbst zu tun, es handelt sich lediglich um einen Platzhalter.", "img/placeholder.png"),
         new Movie("Tom", 2012, ["Tim", "Jim", "Guy"], "Test text, um die Website zu testen. Diese Informationen haben nichts mit dem Film selbst zu tun, es handelt sich lediglich um einen Platzhalter.", "img/placeholder.png")
     ]);
-    this.watchedMovies = ko.observableArray([
+    this.watchedMovies = new MovieList("Nochmal ansehen", [
         new Movie("Tom", 2012, ["Tim", "Jim", "Guy"], "Test text, um die Website zu testen. Diese Informationen haben nichts mit dem Film selbst zu tun, es handelt sich lediglich um einen Platzhalter.", "img/placeholder.png"),
         new Movie("Tom", 2012, ["Tim", "Jim", "Guy"], "Test text, um die Website zu testen. Diese Informationen haben nichts mit dem Film selbst zu tun, es handelt sich lediglich um einen Platzhalter.", "img/placeholder.png"),
         new Movie("Tom", 2012, ["Tim", "Jim", "Guy"], "Test text, um die Website zu testen. Diese Informationen haben nichts mit dem Film selbst zu tun, es handelt sich lediglich um einen Platzhalter.", "img/placeholder.png")
     ]);
+
+    this.page = new Page("Netflix", [this.newMovies, this.popularMovies, this.watchedMovies]);
 }
 
 ko.applyBindings(new NetflixViewModel());
